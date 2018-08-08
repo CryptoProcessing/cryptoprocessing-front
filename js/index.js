ea17d23bca8a6e8a41707389392cf0c9ce4a9f40
fract411 <mtpw@yandex.ru>
2018-08-08 12:56:45 +0300

    add solutions block

--- js/index.js
+++ js/index.js
@@ -1,16 +1,21 @@
-$(document).ready(function(){
+$(document).ready(function() {
   // header start
 
   $('.header__item_with_sublist, .header__switcher').click(function(e) {
     e.preventDefault();
-
     var list = $(this).find('.window');
-    
-    list.is(':visible') ? list.fadeOut() : list.fadeIn();
 
+    list.is(':visible') ? list.fadeOut() : list.fadeIn();
+  });
+
+  $(window).scroll(function() {
+    var top = $(this).scrollTop();
+
+    top > 0 ? $('header').addClass('header_fixed') : false;
+    top === 0 ? $('header').removeClass('header_fixed') : false;
   });
 
   // header end
 
-  
+
 });
