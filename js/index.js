efb10d2455fee8091e2a0e910ad3dd9476a41e10
fract411 <mtpw@yandex.ru>
2018-08-09 00:46:09 +0300

    add particles and contact page

--- js/index.js
+++ js/index.js
@@ -8,11 +8,20 @@ $(document).ready(function() {
     list.is(':visible') ? list.fadeOut() : list.fadeIn();
   });
 
+  $(window).scrollTop() > 0 ? $('header').addClass('header_fixed') : false;
+
   $(window).scroll(function() {
     var top = $(this).scrollTop();
 
     top > 0 ? $('header').addClass('header_fixed') : false;
     top === 0 ? $('header').removeClass('header_fixed') : false;
+  });
+
+  $(document).click(function(e) {
+    var target = $(e.target);
+    if(!target.closest('.header__item_with_sublist, .switcher').length) {
+      $('.window').fadeOut();
+    }
   });
 
   // header end
